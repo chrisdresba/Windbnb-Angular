335a993be4ca5c5c1a0b322be24323bdd72ad18d
chrisdresba <chrisdresba@gmail.com>
2023-03-14 16:51:57 -0300

    ajustes estilo

--- Windbnb/src/environments/environment.prod.ts
+++ Windbnb/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-   /*CLAVE FIREBASE*/
+    apiKey: "AIzaSyDf3wFGRSknVLs8Nm1S3it8H8vpU7-8yak",
+    authDomain: "windbnb-angular.firebaseapp.com",
+    projectId: "windbnb-angular",
+    storageBucket: "windbnb-angular.appspot.com",
+    messagingSenderId: "105578055404",
+    appId: "1:105578055404:web:e337761c44bdc906a9c7c9"
   }
 };
